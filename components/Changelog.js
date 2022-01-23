90fcf1a8ab345ee409fab025a6aa58e6a0be6d92
qwyzex <qwyzex@gmail.com>
2022-01-23 23:45:07 +0700

    fix overflowing description in changelog

--- components/Changelog.js
+++ components/Changelog.js
@@ -26,24 +26,6 @@ const ChangeLog = () => {
                     </li>
                 ))
                 .reverse()}
-            {changelog.forEach((c) => (
-                <li key={c.version} id={c.version} className={styles.changelogItem}>
-                    <h1>{c.version}</h1>
-                    <p className={`cascade ${styles.date}`}>{c.date}</p>
-                    <p className={styles.description}>{c.description}</p>
-                    <details className={styles.featureWrapper}>
-                        <summary>What's New?</summary>
-                        <ul>
-                            {c.feature.map((f) => (
-                                <li key={f.name}>
-                                    <h5>{f.name}</h5>
-                                    <p>{f.description != "" && f.description}</p>
-                                </li>
-                            ))}
-                        </ul>
-                    </details>
-                </li>
-            ))}
         </ul>
     );
 };
