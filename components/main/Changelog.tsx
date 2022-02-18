e737e47a5f83dabf97f867c806bff2a9f126f35a
qwyzex <qwyzex@gmail.com>
2022-02-18 20:28:02 +0700

    Fix: Fatal error with changelog directory

--- components/main/Changelog.tsx
+++ components/main/Changelog.tsx
@@ -1,65 +1,100 @@
-import { useState, useEffect } from 'react';
-import { SpinnerCircular } from 'spinners-react';
-import styles from '/styles/ChangeLog.module.sass';
-import { Hr } from '..';
+import Head from 'next/head';
+import Link from 'next/link';
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
-export interface ChangelogDataprops {
-	version: string;
-	date: string;
-	description: string;
-	feature: string[];
+import { auth } from '../../firebase';
+import styles from '/styles/Changelog.module.sass';
+
+import { SVG, Header, Loading, Hr } from '..';
+import { SpinnerDiamond } from 'spinners-react';
+import { NextPage } from 'next';
+import { getUserData } from '../../functions';
+import { DocumentData } from 'firebase/firestore';
+import { IndexTypes } from 'pages';
+import { ChangelogDataType } from 'pages/api/changelog';
+
+export interface ChangeLogPageProps {
+	setActivePage: IndexTypes['setActivePage'];
 }
 
 const ChangeLog = () => {
-	const [changelogData, setChangelogData]: any = useState([]);
+	const [user] = useAuthState(auth);
+
+	const [changelogData, setChangelogData] = useState<ChangelogDataType[]>();
 
 	useEffect(() => {
 		async function fetchChangelog() {
 			const res: Response = await fetch('/api/changelog');
-			const data: JSON = await res.json();
+			const data = await res.json();
 
 			setChangelogData(data);
 		}
 		fetchChangelog();
 	}, []);
 
+	const ChangeLogList = () => {
+		return (
+			<ul className={styles.wrapper}>
+				{changelogData ? (
+					changelogData
+						.map((c: ChangelogDataType) => (
+							<li
+								key={c.version}
+								id={c.version}
+								className={styles.changelogItem}
+							>
+								<h1>{c.version}</h1>
+								<p className={`cascade ${styles.date}`}>
+									{c.date}
+								</p>
+								<p className={styles.description}>
+									{c.description}
+								</p>
+								<Hr glow />
+								<details className={styles.featureWrapper}>
+									<summary>What{"'"}ve Changes?</summary>
+									<ul>
+										{c.feature.map((f: any) => (
+											<li key={f.name}>
+												<h5>{f.name}</h5>
+												<p>
+													{f.description != '' &&
+														f.description}
+												</p>
+											</li>
+										))}
+									</ul>
+								</details>
+							</li>
+						))
+						.reverse()
+				) : (
+					<Loading />
+				)}
+			</ul>
+		);
+	};
+
 	return (
-		<ul className={styles.wrapper}>
-			{changelogData ? (
-				changelogData
-					.map((c: ChangelogDataprops) => (
-						<li
-							key={c.version}
-							id={c.version}
-							className={styles.changelogItem}
-						>
-							<h1>{c.version}</h1>
-							<p className={`cascade ${styles.date}`}>{c.date}</p>
-							<p className={styles.description}>
-								{c.description}
-							</p>
-							<Hr glow />
-							<details className={styles.featureWrapper}>
-								<summary>What{"'"}ve Changes?</summary>
-								<ul>
-									{c.feature.map((f: any) => (
-										<li key={f.name}>
-											<h5>{f.name}</h5>
-											<p>
-												{f.description != '' &&
-													f.description}
-											</p>
-										</li>
-									))}
-								</ul>
-							</details>
-						</li>
-					))
-					.reverse()
-			) : (
-				<SpinnerCircular />
-			)}
-		</ul>
+		<div>
+			<Head>
+				<title>AXC Changelog</title>
+				<meta name="descripton" content="AXC Life Changelog"></meta>
+			</Head>
+			<main className={styles.container}>
+				<Link href="/">
+					<a>
+						{user ? '<< Back To Chatroom' : '<< Back To Homepage'}
+					</a>
+				</Link>
+				<div>
+					<SVG.TimeReverse />
+					<h1>Changelog</h1>
+				</div>
+				{changelogData ? <ChangeLogList /> : <SpinnerDiamond />}
+			</main>
+		</div>
 	);
 };
 
--- pages/changelog.tsx
+++ pages/changelog.tsx
@@ -1,56 +1,22 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import { useState, useEffect } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-
-import { auth } from '../firebase';
-import styles from '/styles/ChangelogPage.module.sass';
-
-import { SVG, Header, ChangeLog } from '../components';
-import { SpinnerDiamond } from 'spinners-react';
+import { ChangeLog, Header } from '../components';
 import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 import { getUserData } from '../functions';
 import { DocumentData } from 'firebase/firestore';
 
-const ChangeLogPage: NextPage = () => {
-	const [user] = useAuthState(auth);
-
-	const [changelogData, setChangelogdata] = useState(null);
+const ChangelogPage: NextPage = () => {
 	const [userData, setUserData] = useState<DocumentData>({});
 
 	useEffect(() => {
 		getUserData(setUserData);
 	}, [userData]);
 
-	useEffect(() => {
-		async function fetchChangelog() {
-			const res: Response = await fetch('/api/changelog');
-			const data = await res.json();
-
-			setChangelogdata(data);
-		}
-		fetchChangelog();
-	}, []);
-
 	return (
-		<div>
-			<Head>
-				<title>App Changelog</title>
-				<meta name="descripton" content="AXC Life Changelog"></meta>
-			</Head>
+		<>
 			<Header userData={userData} setActivePage={() => {}} />
-			<main className={styles.container}>
-				<Link href="/">
-					{user ? '<< Back To Chatroom' : '<< Back To Homepage'}
-				</Link>
-				<div>
-					<SVG.TimeReverse />
-					<h1>Changelog</h1>
-				</div>
-				{changelogData ? <ChangeLog /> : <SpinnerDiamond />}
-			</main>
-		</div>
+			<ChangeLog />
+		</>
 	);
 };
 
-export default ChangeLogPage;
+export default ChangelogPage;
