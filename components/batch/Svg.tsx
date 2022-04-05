6ddc48f76630314bf628559fe30abc0f2110467b
qwyzex <qwyzex@gmail.com>
2022-04-05 19:15:50 +0700

    Add: Check SVG Component

--- components/batch/Svg.tsx
+++ components/batch/Svg.tsx
@@ -115,6 +115,19 @@ export function TimeReverse() {
 	);
 }
 
+export function Check() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+		>
+			<path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z" />
+		</svg>
+	);
+}
+
 const SVG = {
 	Dollar,
 	Info,
@@ -125,6 +138,7 @@ const SVG = {
 	Account,
 	Bug,
 	TimeReverse,
+	Check,
 };
 
 export default SVG;
